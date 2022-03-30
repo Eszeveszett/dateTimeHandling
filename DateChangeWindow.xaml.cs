71eefbc852d7404699e0a469b941f08a04904c3d
Miklós Gruber <gyemantlelek@outlook.com>
2022-03-30 10:42:38 +0200

    Add project files.

---
+++ DateChangeWindow.xaml.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace dateTimeHandling
+{
+    /// <summary>
+    /// Interaction logic for DateChangeWindow.xaml
+    /// </summary>
+    public partial class DateChangeWindow : Window
+    {
+        int year;
+        int month;
+        int day;
+        int hour;
+        int minute;
+        int second;
+        public DateChangeWindow()
+        {
+            InitializeComponent();
+            TBO_Date.Text = DateTime.Now.ToString();
+            year = DateTime.Now.Year;
+            month = DateTime.Now.Month;
+            day = DateTime.Now.Day;
+            hour = DateTime.Now.Hour;
+            minute = DateTime.Now.Minute;
+            second = DateTime.Now.Second;
+        }
+
+        private void BTN_Change_Click(object sender, RoutedEventArgs e)
+        {
+            int ChangedTimeValue = 0;
+            TBO_ChangedDate.Text = DateTime.Now.ToString();
+
+            if (year != Convert.ToInt32(Convert.ToDateTime(TBO_ChangedDate.Text).Year))
+            {
+                year++;
+            }
+            else
+            {
+                if (month != Convert.ToInt32(Convert.ToDateTime(TBO_ChangedDate.Text).Month))
+                {
+                    month++;
+                }
+                else
+                {
+                    if (day != Convert.ToInt32(Convert.ToDateTime(TBO_ChangedDate.Text).Day))
+                    {
+                        day++;
+                    }
+                    else
+                    {
+                        if (hour != Convert.ToInt32(Convert.ToDateTime(TBO_ChangedDate.Text).Hour))
+                        {
+                            ChangedTimeValue = Convert.ToInt32(Convert.ToDateTime(TBO_ChangedDate.Text).Hour) - hour;
+                            hour += ChangedTimeValue;
+                            if (hour > 59)
+                            {
+                                hour = DateTime.Now.Hour;
+                            }
+                        }
+                        else
+                        {
+                            if (minute != Convert.ToInt32(Convert.ToDateTime(TBO_ChangedDate.Text).Minute))
+                            {
+                                ChangedTimeValue = Convert.ToInt32(Convert.ToDateTime(TBO_ChangedDate.Text).Minute) - minute;
+                                minute += ChangedTimeValue;
+                                if (minute > 59)
+                                {
+                                    minute = DateTime.Now.Minute;
+                                }
+                            }
+                            else
+                            {
+                                if (second != Convert.ToInt32(Convert.ToDateTime(TBO_ChangedDate.Text).Second))
+                                {
+                                    ChangedTimeValue = Convert.ToInt32(Convert.ToDateTime(TBO_ChangedDate.Text).Second)-second;
+                                    second += ChangedTimeValue;
+                                    if (second > 59)
+                                    {
+                                        second = DateTime.Now.Second;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            TBO_EndDate.Text = year.ToString() +" "+ month.ToString() +" "+ day.ToString() +" "+ hour.ToString() + " " + minute.ToString() + " " + second.ToString();
+
+            if (string.IsNullOrEmpty(TBO_StartYear.Text))
+            {
+                DPI_StartDate.IsDropDownOpen = true;
+            }
+            else
+            {
+                if (string.IsNullOrEmpty(TBO_ChangedYear.Text))
+                {
+                    DPI_ChangedDate.IsDropDownOpen= true;
+                }
+                else
+                {
+                    //TBO_EndYear.Text = (Convert.ToInt32(TBO_ChangedYear.Text) - Convert.ToInt32(TBO_StartYear.Text)).ToString();
+
+                    /*Számolás hónapokkal*/
+                    if (Convert.ToInt32(TBO_StartMonth.Text) + Convert.ToInt32(TBO_ChangedMonth.Text) <= DateTime.MaxValue.Month)
+                    {
+                        TBO_EndMonth.Text = (Convert.ToInt32(TBO_ChangedMonth.Text) - Convert.ToInt32(TBO_StartMonth.Text)).ToString();
+                        if (Convert.ToInt32(TBO_StartDay.Text) + Convert.ToInt32(TBO_ChangedDay.Text) <= DateTime.MaxValue.Day)
+                        {
+                            TBO_EndDay.Text = (Convert.ToInt32(TBO_ChangedDay.Text) - Convert.ToInt32(TBO_StartDay.Text)).ToString();
+                        }
+                    }
+                    else
+                    {
+                        if (Convert.ToInt32(TBO_StartDay.Text) + Convert.ToInt32(TBO_ChangedDay.Text) <= DateTime.MaxValue.Day)
+                        {
+                            
+                            TBO_EndDay.Text = (Convert.ToInt32(TBO_ChangedDay.Text) - Convert.ToInt32(TBO_StartDay.Text)).ToString();
+                        }
+                        else
+                        {
+                            TBO_EndMonth.Text = (Convert.ToInt32(TBO_EndMonth.Text) + 1).ToString();
+                        }
+                    }
+                    
+                }
+            }
+        }
+
+        private void DPI_StartDate_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            TBO_StartYear.Text = Convert.ToDateTime(DPI_StartDate.Text).Year.ToString();
+            TBO_StartMonth.Text = Convert.ToDateTime(DPI_StartDate.Text).Month.ToString();
+            TBO_StartDay.Text = Convert.ToDateTime(DPI_StartDate.Text).Day.ToString();
+        }
+
+        private void DPI_ChangedDate_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            TBO_ChangedYear.Text = Convert.ToDateTime(DPI_ChangedDate.Text).Year.ToString();
+            TBO_ChangedMonth.Text = Convert.ToDateTime(DPI_ChangedDate.Text).Month.ToString();
+            TBO_ChangedDay.Text = Convert.ToDateTime(DPI_ChangedDate.Text).Day.ToString();
+        }
+
+        private void BTN_Quit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+            Application.Current.MainWindow.Close();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace dateTimeHandling
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DateChangeWindow DCW = new DateChangeWindow();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //DateTime most = DateTime.Now;
+
+            TBO_ActualDate.Text = DateTime.Now.Date.ToString();
+
+            DCW.ShowDialog();
+        }
+
+        private void BTN_Refresh_Click(object sender, RoutedEventArgs e)
+        {
+            TBO_ActualDate.Text = DateTime.Now.ToString();
+
+            TBO_ActualYear.Text = DateTime.Now.Year.ToString();
+            TBO_ActualMonth.Text = DateTime.Now.Month.ToString();
+            TBO_ActualMonthString.Text = DateTime.Now.ToString("MMMM");
+            TBO_ActualDay.Text = DateTime.Now.Day.ToString();
+            TBO_ActualDayString.Text = DateTime.Now.ToString("dddd");
+            TBO_ActualHour.Text = DateTime.Now.Hour.ToString();
+            TBO_ActualMinute.Text = DateTime.Now.Minute.ToString();
+            TBO_ActualSecond.Text = DateTime.Now.Second.ToString();
+        }
+
+        private void BTN_DateChange_Click(object sender, RoutedEventArgs e)
+        {
+            //DCW.ShowDialog();
+        }
+
+        private void BTN_Close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+
+    }
+}
